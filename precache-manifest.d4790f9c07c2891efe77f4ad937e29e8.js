67001f24f6d5ee95ff7554b5e0c65999b33cb7eb
Artem Meges <artemmeges@gmail.com>
2020-09-11 19:35:20 +0300

    Updates

---
+++ precache-manifest.d4790f9c07c2891efe77f4ad937e29e8.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a083d88e345ad147adc3265f99b0c3a",
+    "url": "/react-pokemon-app/index.html"
+  },
+  {
+    "revision": "613a6f1926d37daac9a1",
+    "url": "/react-pokemon-app/static/css/main.ff321d2c.chunk.css"
+  },
+  {
+    "revision": "e6cc4f2a9bfb0aa862b9",
+    "url": "/react-pokemon-app/static/js/2.71dc91e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-pokemon-app/static/js/2.71dc91e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "613a6f1926d37daac9a1",
+    "url": "/react-pokemon-app/static/js/main.0d3f3c5b.chunk.js"
+  },
+  {
+    "revision": "d44f2446001b93edb714",
+    "url": "/react-pokemon-app/static/js/runtime-main.24cf928f.js"
+  },
+  {
+    "revision": "293987a735e6c4537c1877baae89d371",
+    "url": "/react-pokemon-app/static/media/bug.293987a7.svg"
+  },
+  {
+    "revision": "1c32b1c40a9aae5a3ea42feefccbec2a",
+    "url": "/react-pokemon-app/static/media/dark.1c32b1c4.svg"
+  },
+  {
+    "revision": "c5b2a3d6c99e44859b8a4bd4b2a3b1ef",
+    "url": "/react-pokemon-app/static/media/dragon.c5b2a3d6.svg"
+  },
+  {
+    "revision": "472e31a063028bd0a6cb4788c5ee64f7",
+    "url": "/react-pokemon-app/static/media/electric.472e31a0.svg"
+  },
+  {
+    "revision": "7c252efeb9e290f262dc8b23eae6b77c",
+    "url": "/react-pokemon-app/static/media/fairy.7c252efe.svg"
+  },
+  {
+    "revision": "f5b4209f8c212497a0b13629b0e1108c",
+    "url": "/react-pokemon-app/static/media/fighting.f5b4209f.svg"
+  },
+  {
+    "revision": "ab0f0303920b62b2c01b557eb8b3bd3c",
+    "url": "/react-pokemon-app/static/media/fire.ab0f0303.svg"
+  },
+  {
+    "revision": "498a5dbeef97b03c04f1bacd8cf10b35",
+    "url": "/react-pokemon-app/static/media/flying.498a5dbe.svg"
+  },
+  {
+    "revision": "57af7c688f51bbe2c7eafe19a34b6050",
+    "url": "/react-pokemon-app/static/media/ghost.57af7c68.svg"
+  },
+  {
+    "revision": "85f2ac72fcd454e1a49b6de9d7754f79",
+    "url": "/react-pokemon-app/static/media/grass.85f2ac72.svg"
+  },
+  {
+    "revision": "d7075b6821435aadfad9141efa54be19",
+    "url": "/react-pokemon-app/static/media/ground.d7075b68.svg"
+  },
+  {
+    "revision": "8dda91fca1e47ab97457c000daf9763e",
+    "url": "/react-pokemon-app/static/media/ice.8dda91fc.svg"
+  },
+  {
+    "revision": "ac1976674b4b15f7f10efc496722ed3b",
+    "url": "/react-pokemon-app/static/media/loading.ac197667.svg"
+  },
+  {
+    "revision": "edc201c5c8e5b1ff5a8034cadd2aa64e",
+    "url": "/react-pokemon-app/static/media/normal.edc201c5.svg"
+  },
+  {
+    "revision": "47a1e0134183a568b889c11de4871b36",
+    "url": "/react-pokemon-app/static/media/poison.47a1e013.svg"
+  },
+  {
+    "revision": "21bcc80fed49942792dfa58acb11d443",
+    "url": "/react-pokemon-app/static/media/psychic.21bcc80f.svg"
+  },
+  {
+    "revision": "0080e38fb02e936bd3fd2e7ffccd94fb",
+    "url": "/react-pokemon-app/static/media/rock.0080e38f.svg"
+  },
+  {
+    "revision": "7a9b1c158427c197b3213ecce25f57d5",
+    "url": "/react-pokemon-app/static/media/shadow.7a9b1c15.svg"
+  },
+  {
+    "revision": "99db0044685537822ac4b6a6c45a1b0e",
+    "url": "/react-pokemon-app/static/media/steel.99db0044.svg"
+  },
+  {
+    "revision": "a843fd2d276dd7e547b650051f5d96be",
+    "url": "/react-pokemon-app/static/media/water.a843fd2d.svg"
+  }
+]);
